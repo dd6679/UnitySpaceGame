a57a0774c5d69b1f714dd9f73d7c41a8dc2349c1
dd6679 <dd6679@naver.com>
2021-02-12 04:16:55 +0900

    Add gitattributes

---
+++ Assets/BallSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterSpawn;
+    private int i = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Shot()
+    {
+        Transform ac = transform.GetChild(i);
+
+        GameObject bullet = Instantiate(bulletPrefab, new Vector3(0, -1.66f, 3.84f), transform.rotation);
+
+        target = ac.transform;
+        bullet.transform.LookAt(target);
+
+        i += 1;
+   
+    }
+}
---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
